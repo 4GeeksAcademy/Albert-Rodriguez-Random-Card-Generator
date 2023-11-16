b797eb59697fbc597e016b29166015776397f330
Albert Rodriguez <rdrgz1992@gmail.com>
2023-11-16 01:56:07 +0000

    Made the card spin

--- src/app.js
+++ src/app.js
@@ -7,39 +7,47 @@ window.onload = function() {
     getRandomCard();
   });
 };
-//getRandomCard();
+
 function getRandomCard() {
-  const suits = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
-  const cards = [
-    "A",
-    "J",
-    "Q",
-    "K",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10"
-  ];
+  const cardElement = document.querySelector(".card");
 
-  let randomSuit = suits[Math.floor(Math.random() * suits.length)];
-  let randomCard = cards[Math.floor(Math.random() * cards.length)];
+  cardElement.classList.add("spin-animation");
 
-  document.getElementById("top-suit").innerHTML = randomSuit;
-  document.getElementById("bottom-suit").innerHTML = randomSuit;
-  document.getElementById("card-number").innerHTML = randomCard;
+  setTimeout(() => {
+    cardElement.classList.remove("spin-animation");
 
-  if (randomSuit === "&hearts;" || randomSuit === "&diams;") {
-    document.getElementById("top-suit").style.color = "red";
-    document.getElementById("bottom-suit").style.color = "red";
-    document.getElementById("card-number").style.color = "red";
-  } else {
-    document.getElementById("top-suit").style.color = "black";
-    document.getElementById("bottom-suit").style.color = "black";
-    document.getElementById("card-number").style.color = "black";
-  }
+    const suits = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+    const cards = [
+      "A",
+      "J",
+      "Q",
+      "K",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10"
+    ];
+
+    let randomSuit = suits[Math.floor(Math.random() * suits.length)];
+    let randomCard = cards[Math.floor(Math.random() * cards.length)];
+
+    document.getElementById("top-suit").innerHTML = randomSuit;
+    document.getElementById("bottom-suit").innerHTML = randomSuit;
+    document.getElementById("card-number").innerHTML = randomCard;
+
+    if (randomSuit === "&hearts;" || randomSuit === "&diams;") {
+      document.getElementById("top-suit").style.color = "red";
+      document.getElementById("bottom-suit").style.color = "red";
+      document.getElementById("card-number").style.color = "red";
+    } else {
+      document.getElementById("top-suit").style.color = "black";
+      document.getElementById("bottom-suit").style.color = "black";
+      document.getElementById("card-number").style.color = "black";
+    }
+  }, 500);
 }
